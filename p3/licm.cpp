31dd2778e88e97092f6ed817634a62352069b1f5
Lamasu <rafaerthal@gmail.com>
2014-12-17 14:03:05 -0200

    final adjustments

--- p3/licm.cpp
+++ p3/licm.cpp
@@ -1,5 +1,4 @@
 #include "llvm/Pass.h"
-#include "llvm/IR/Dominators.h"
 #include "llvm/Analysis/LoopPass.h"
 #include "llvm/Analysis/ValueTracking.h"
 #include "llvm/Transforms/Scalar.h"
@@ -13,45 +12,46 @@ namespace {
     LICM() : LoopPass(ID) {}
     bool runOnLoop(Loop *L, LPPassManager &LPM) override;
     virtual void getAnalysisUsage(AnalysisUsage &AU) const {
-      AU.addRequired<DominatorTreeWrapperPass>();
       AU.addRequiredID(LoopSimplifyID);
+      AU.addPreservedID(LoopSimplifyID);
     };
   };
 }
 
 bool LICM::runOnLoop(Loop *loop, LPPassManager &LPM) {
-  Loop::block_iterator block;
-  BasicBlock::iterator instruction;
   std::vector<Instruction *> loopInvariants;
   bool changedOnLoop, instructionIsLoopInvariant;
+  BasicBlock * PreHeader = loop->getLoopPreheader();
 
   do {
     changedOnLoop = false;
-    for (block = loop->block_begin(); block != loop->block_end(); ++block) {
-      for (instruction = (*block)->begin(); instruction != (*block)->end(); ++instruction) {
+
+    for (Loop::block_iterator block = loop->block_begin(); block != loop->block_end(); ++block) {
+      for (BasicBlock::iterator instruction = (*block)->begin(); instruction != (*block)->end(); ++instruction) {
         instructionIsLoopInvariant = true;
 
-        for (int i = 0; i < loopInvariants.size(); i++) {
+        for (int i = 0; i < loopInvariants.size(); i++)
           if (loopInvariants[i] == instruction) instructionIsLoopInvariant = false;
-        }
 
-        for (int i = 0; i < instruction->getNumOperands(); i++) {
-          Instruction * ins = dyn_cast<Instruction>(instruction->getOperand(i));
-          Constant    * con = dyn_cast<Constant>(instruction->getOperand(i));
-          Argument    * arg = dyn_cast<Argument>(instruction->getOperand(i));
+        if (instruction->mayReadOrWriteMemory()) break;
+        if (!isSafeToSpeculativelyExecute(instruction)) break;
+
+        for (Instruction::op_iterator operand = instruction->op_begin(); operand != instruction->op_end(); ++operand) {
+          Instruction * ins = dyn_cast<Instruction>(operand);
+
           if (ins) {
-            bool belongsToLoop   = false;
+            bool belongsToLoop = false;
             bool isLoopInvariant = false;
-            for (Loop::block_iterator childBlock = loop->block_begin(); childBlock != loop->block_end(); ++childBlock) {
+
+            for (Loop::block_iterator childBlock = loop->block_begin(); childBlock != loop->block_end(); ++childBlock)
               if (ins->getParent() == *childBlock) belongsToLoop = true;
-            }
-            for (int i = 0; i < loopInvariants.size(); i++) {
+
+            for (int i = 0; i < loopInvariants.size(); i++)
               if (loopInvariants[i] == ins) isLoopInvariant = true;
-            }
+
             if (belongsToLoop && !isLoopInvariant) instructionIsLoopInvariant = false;
-          } else if (!con && !arg) {
-            instructionIsLoopInvariant = false;
-          }
+
+          } else if (!dyn_cast<Constant>(operand) && ! dyn_cast<Argument>(operand)) instructionIsLoopInvariant = false;
         }
         if (instructionIsLoopInvariant) {
           loopInvariants.push_back(instruction);
@@ -61,25 +61,9 @@ bool LICM::runOnLoop(Loop *loop, LPPassManager &LPM) {
     }
   } while (changedOnLoop);
 
-  BasicBlock * PreHeader = loop->getLoopPreheader();
-  DominatorTree *DT = &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
-  SmallVector<BasicBlock *, 8> exitBlocks;
-  loop->getExitBlocks(exitBlocks);
-  bool changed = false;
-
-  for (int i = 0; i < loopInvariants.size(); i++) {
-    bool dominates = true;
-    for (int j = 0; j < exitBlocks.size(); j ++) {
-      if (!DT->dominates(loopInvariants[i]->getParent(), exitBlocks[j])) {
-        dominates = false;
-      }
-    }
-    if (dominates) {
-      changed = true;
-      loopInvariants[i]->moveBefore(PreHeader->getTerminator());
-    }
-  }
-  return changed;
+  for (int i = 0; i < loopInvariants.size(); i++) loopInvariants[i]->moveBefore(PreHeader->getTerminator());
+  errs() << "foram movidas " << loopInvariants.size() << " invariantes\n";
+  return loopInvariants.size() > 0;
 }
 
 char LICM::ID = 0;
